16b4cb1e44db510cf60d19650d32198b794cf008
bapt2 <baptiste18c.touraud@gmail.com>
2023-07-24 16:03:08 +0200

    Play with strings

---
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,12 @@
+﻿﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str1 = "Holberton";
+		string str2 = "School";
+		string str3 = " ";
+		Console.WriteLine("Welcome to {0}!", str1 + str3 + str2);
+	}
+}

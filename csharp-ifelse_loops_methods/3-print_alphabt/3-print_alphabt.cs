568d4936cab469df04dcb3f2483f3fd7ca089bf6
bapt2 <baptiste18c.touraud@gmail.com>
2023-07-29 22:05:04 +0200

    print alphabt

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            if (i != 5 && i != 17)
+            {
+                Console.Write(Convert.ToChar(i + (int)'a'));
+            }
+        }
+    }
+}

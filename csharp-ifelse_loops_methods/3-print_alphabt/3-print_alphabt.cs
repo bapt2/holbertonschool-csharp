9091f0452c107aa5de9a685c08aedb45323f0c35
bapt2 <baptiste18c.touraud@gmail.com>
2023-07-29 22:08:45 +0200

    fixing unexpected issue

--- csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -6,7 +6,7 @@ class Program
     {
         for (int i = 0; i < 26; i++)
         {
-            if (i != 5 && i != 17)
+            if (i != 6 && i != 17)
             {
                 Console.Write(Convert.ToChar(i + (int)'a'));
             }

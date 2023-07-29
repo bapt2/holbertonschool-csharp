71c29e8c8cff69052b9c39822bb3c6faabf2d0b7
bapt2 <baptiste18c.touraud@gmail.com>
2023-07-29 21:53:13 +0200

    print alphabet

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            Console.Write(Convert.ToChar(i + (int)'a'));
+        }
+    }
+}

ae60b4926b5eea0cdf5779d36b071768227fd341
bapt2 <baptiste18c.touraud@gmail.com>
2023-07-29 22:40:49 +0200

    Hexadecimal printing

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 98; i++)
+            {
+                Console.Write("{0} = {1}\n", i, i.ToString("X"));
+            }
+        }
+    }
+}

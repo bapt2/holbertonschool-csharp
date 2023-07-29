362fe1034322ddd0bd409684d5613f13a83c6976
bapt2 <baptiste18c.touraud@gmail.com>
2023-07-29 20:25:57 +0200

    fixing a wrong startingindex

--- csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
+++ csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -5,7 +5,7 @@ class Program
         static void Main(string[] args)
         {
 		string word = "Holberton";
-		string word_first_3 =  word.Substring(1, 3);
+		string word_first_3 =  word.Substring(0, 3);
         string word_last_2 = word.Substring(7);
         string middle_word = word.Substring(1, 7);
 		Console.WriteLine("First 3 letters: " + word_first_3);

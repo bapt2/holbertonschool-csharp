9723102693b14136b8df885ad11c3d30c6e2c2c8
bapt2 <baptiste18c.touraud@gmail.com>
2023-07-30 10:54:09 +0200

    PrintDiagonal

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            for (int i = 1; i <= length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("/");
+            }
+        }
+    }

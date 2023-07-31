4bb1531148f1a00040247d0d64e9d09a9fc74d29
bapt2 <baptiste18c.touraud@gmail.com>
2023-07-31 16:56:02 +0200

    print an array in reverse

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Array
+    {
+        public static void Reverse(int[] array)
+        {
+            if (array != null && array.Length > 0)
+            {
+                for (int i = array.Length - 1; i >= 0; i--)
+                {
+                    if (i != 0)
+                    {
+                        Console.Write("{0} ", array[i]);
+                    }
+                    else
+                    {
+                        Console.WriteLine(array[i]);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }

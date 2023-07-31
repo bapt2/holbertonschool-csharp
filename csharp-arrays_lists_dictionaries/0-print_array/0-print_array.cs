36730cc47dd0e0e7c5706413e184dbea151a6a0d
bapt2 <baptiste18c.touraud@gmail.com>
2023-07-31 15:09:40 +0200

    print an array of integers

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            int[] Array = new int[size];
+            if (size == 0)
+            {
+                Console.WriteLine();
+                return Array;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                if (i != size - 1)
+                {
+                    Console.Write("{0} ", i);
+                }
+                else 
+                {
+                    Console.Write(i);
+                }
+            }
+
+            return Array;
+        }
+    }

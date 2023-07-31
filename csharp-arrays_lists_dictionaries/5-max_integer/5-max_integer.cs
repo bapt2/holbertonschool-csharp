74b9b20534f8c15286f372ca4c9e8a32ce3090b9
bapt2 <baptiste18c.touraud@gmail.com>
2023-07-31 17:38:52 +0200

    find max number

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+    class List
+    {
+        public static int MaxInteger(List<int> myList)
+        {
+            if(myList == null)
+            {
+                Console.WriteLine("List is empty");
+                return -1;
+            }
+            myList.Sort();
+            int i = 0;
+            for (; i < myList.Count; i++)
+            {
+                if(i == myList.Count - 1)
+                {
+                    return myList[i];
+                }
+            }
+            return i;
+        }
+    }

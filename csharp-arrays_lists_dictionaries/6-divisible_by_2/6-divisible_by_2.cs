ca6b244e72a8a13c1936096e8e9a8d3aa9cc3bae
bapt2 <baptiste18c.touraud@gmail.com>
2023-07-31 17:51:39 +0200

    is divisible by 2

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<bool> DivisibleBy2(List<int> myList)
+        {
+            List<bool> isDivisibleBy2 = new List<bool>();
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (myList[i] % 2 == 0)
+                {
+                    isDivisibleBy2.Add(true);
+                }
+                else
+                {
+                    isDivisibleBy2.Add(false);
+                }
+            }
+            return isDivisibleBy2;
+        }
+    }

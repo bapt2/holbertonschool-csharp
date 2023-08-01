acb0fbd04149cb8ae388549603b56a54fe505e31
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-01 14:57:36 +0200

    two dimensional array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] square = new int[5,5];
+            for (int i = 0; i < square.Length; i++)
+            {
+                if (i == 5 || i == 10 || i == 15 || i == 20 || i == 25)
+                {
+                    Console.WriteLine();
+                }
+                if (i == 12)
+                {
+                Console.Write("1");
+                }
+                else
+                {
+                Console.Write("0");
+                }
+            }
+            Console.WriteLine();
+
+        }
+    }
+

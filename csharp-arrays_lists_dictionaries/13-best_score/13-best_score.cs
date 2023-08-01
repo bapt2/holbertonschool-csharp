e7240afef2965c6b8d020db4ddda816bbd64d806
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-01 14:41:36 +0200

    return the key with the biggest value

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static string BestScore(Dictionary<string, int> myList)
+        {
+            int score = -1;
+            string none = "None";
+            foreach (KeyValuePair<string, int> kvp in myList)
+            {
+                if (kvp.Value > score)  
+                {
+                    score = kvp.Value;
+                    none = kvp.Key;
+                }              
+            }
+            return none;
+        }
+    }

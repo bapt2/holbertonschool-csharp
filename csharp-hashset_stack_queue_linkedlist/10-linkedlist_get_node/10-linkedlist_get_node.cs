fe569e1b6f44afe11b217125031dc31f58931111
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-02 21:22:46 +0200

    get node at index

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current = myLList.First;
+        for (int i = 0; current != null; i++)
+        {
+            current = current.Next;
+            if (i == n -1)
+            {
+                return current.Value;
+            }
+        }
+        return 0;
+    }
+}

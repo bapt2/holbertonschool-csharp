3f6c7a45c2d1649ed8b7d18f74e404f0739713c3
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-02 13:33:40 +0200

    Full stack

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int count = 0;
+        bool isPresent = false;
+        if (aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        for (int i = 0; i < aStack.Count; i++)
+        {
+            count += 1;
+            if (i == aStack.Count - 1)
+            {
+                Console.WriteLine("Number of items: {0}", count);
+                Console.WriteLine("Top items: {0}", aStack.Peek());
+            }
+        }
+        if (aStack.Contains(search))
+        {
+            isPresent = true;
+            Console.WriteLine("Stack contains \"{0}\": {1}", search, isPresent);
+            while (aStack.Count > 0)
+            {
+                string currentItem = aStack.Peek();
+                aStack.Pop();
+                if (currentItem == search)
+                {
+                    break;
+                }
+            }
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}

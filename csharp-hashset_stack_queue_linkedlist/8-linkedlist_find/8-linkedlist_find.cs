0bb3b8c6f2eb662f89bad57c21ef95f579989be4
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-02 18:24:53 +0200

    find value

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        LinkedListNode<int> tmp = myLList.First;
+        for (int i = 0; tmp != null; i++)
+        {
+            if (tmp.Value == value)
+            {
+                return i;
+            }
+            tmp = tmp.Next;
+        }
+        return -1;
+    }
+}
+

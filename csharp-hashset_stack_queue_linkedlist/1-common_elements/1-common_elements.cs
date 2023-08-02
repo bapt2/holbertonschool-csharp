8740b2802d3a0999c0e9fb51312a06af9a92bcd2
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-02 09:52:05 +0200

    Present in both

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> CommonElements(List<int> list1, List<int> list2)
+        {
+            List<int> newList = new List<int>();
+
+            newList = list1.FindAll(x => list2.Contains(x));
+            newList.Sort();
+
+            return newList;
+        }
+    }

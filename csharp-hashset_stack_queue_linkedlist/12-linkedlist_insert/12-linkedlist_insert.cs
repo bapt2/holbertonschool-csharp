3abb5847ac2d759ca7418eb46e623d9c44556683
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-02 22:14:46 +0200

    Insert in order

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current = myLList.First;
+        for (int i = 0; current != null; i++)
+        {
+            if (n < current.Value)
+            {
+                myLList.AddBefore(current, n);
+                return current;
+            }
+            else
+            {
+                current = current.Next;
+            }
+        }
+        return current;
+    }
+}

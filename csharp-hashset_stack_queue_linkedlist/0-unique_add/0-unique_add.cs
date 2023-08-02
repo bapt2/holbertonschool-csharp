54fbb4f6276cfd24ce05f83e6e847664caf3dbb0
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-02 09:23:48 +0200

    adds all unique integers in a list

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            myList = myList.Distinct().ToList();
+            int result = 0;
+            foreach (int unique in myList)
+            {
+                result += unique;
+            }
+            return result;
+        }
+    }
+

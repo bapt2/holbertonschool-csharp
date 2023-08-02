9a2662695f720482fbd2f92f534f242a2ac162a4
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-02 21:57:06 +0200

    linkedlist sum

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static int Sum(LinkedList<int> myLList)
+        {
+            int result = 0;
+            LinkedListNode<int> current = myLList.First;
+            for (int i = 0; current != null; i++)
+            {
+                result += current.Value;
+                current = current.Next;
+            }
+            return result;
+        }
+    }
+

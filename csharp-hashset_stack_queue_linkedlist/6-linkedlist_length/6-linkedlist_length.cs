aa23ea91ff9ec4385206ae51c97af343fc8f7b3c
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-02 17:42:42 +0200

    linkedlist length

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        LinkedListNode<int> length = myLList.First;
+        int count = 0;
+        for (int i = 1;  length!= null; i++)
+        {
+            length = length.Next;
+            count = i;
+        }
+        return count;
+    }
+}
+

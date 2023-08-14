27a37e7caa90062c945b1852deec1023e775f609
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-14 10:50:49 +0200

    define a new struct

---
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}

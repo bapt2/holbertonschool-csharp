578fab770aebb283fdb3a3aeaf48bb10d3a1f3f5
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-14 11:18:17 +0200

    override ToString

---
+++ csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string names, float ages, string owners, Rating ratings)
+    {
+        name = names;
+        age = ages;
+        owner = owners;
+        rating = ratings;
+
+    }
+
+    public override string ToString()
+    {
+        return string.Format("Name: {0}\nAge: {1}\nOwner: {2}\nRating: {3}", name, age, owner, rating);
+    }
+}

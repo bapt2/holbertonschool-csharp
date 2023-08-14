32f9ffe7be4943b6bc933c52d5b5d3913367b7a6
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-14 09:16:12 +0200

    method that prints n elements of a list.

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            try
+            {
+                count += 1;
+                Console.WriteLine(myList[i]);
+            }
+            catch (Exception)
+            {
+            }
+        }
+        return count;
+    }
+}

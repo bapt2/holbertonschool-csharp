693fda8ed91020db2a2a39823b0622bd6b78507e
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-21 08:51:35 +0200

    adding xml comment and a public fieald

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    ///public class Zombie
+    ///</summary>
+    public class Zombie
+    {
+        ///<summary>
+        ///public int health;
+        ///</summary>
+        public int health = 0;
+
+        static void Enemies(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

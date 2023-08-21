fbe798d49249d9e699f4efe669c951f9914c1ef5
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-21 18:45:49 +0200

    adding new test

--- csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -12,13 +12,19 @@ namespace MyMath.Tests
             Assert.AreEqual(MyMath.Operations.Max(list), 50);
             List<int> list1 = new List<int>() {9, 2, 7};
             Assert.AreEqual(MyMath.Operations.Max(list1), 9);
-            List<int> list2 = new List<int>() {3, 7, -5, 15};
+            List<int> list2 = new List<int>() {3, 7, 15};
             Assert.AreEqual(MyMath.Operations.Max(list2), 15);
         }
         public void FindMaxEmpty()
         {
-            List<int> list = new List<int>() {};
+            List<int> list = new List<int>() { };
             Assert.AreEqual(MyMath.Operations.Max(list), 0);
         }
+        public void FindMaxNegativ()
+        {
+            List<int> list = new List<int>() {-1, -2, -3, -4, -5};
+            Assert.AreEqual(MyMath.Operations.Max(list), -1);
+        }
+        
     }
 }

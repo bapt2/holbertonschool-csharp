f31bc2564f3c30aecb3826f284d73c253b43fe87
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-21 08:33:40 +0200

    adding empty class

---
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enemies
+{
+    class Zombie 
+    {
+        static void Enemies(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

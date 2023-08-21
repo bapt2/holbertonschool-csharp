4b6611ee956584faed79783ae124b0ec299476c2
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-21 09:04:54 +0200

    adding condition

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    ///public class Zombie
+    ///</summary>
+    public class Zombie
+    {
+        ///<summary>
+        ///public int health;
+        ///</summary>
+        public int health = 0;
+        /// <summary>
+        /// Zombie
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value< 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+    }
+}

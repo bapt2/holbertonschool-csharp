0162ee737e3114935e9634b215f67f2057ab45cd
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-21 09:44:16 +0200

    adding Name public property

---
+++ csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    ///public class Zombie
+    ///</summary>
+    public class Zombie
+    {
+        int health;
+        string name = "(no name)";
+        /// <summary>
+        /// public string Name
+        /// </summary>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        /// <summary>
+        /// public Zombie()
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>
+        /// Zombie
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+        /// <summary>
+        /// public int GetHealth()
+        /// </summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}

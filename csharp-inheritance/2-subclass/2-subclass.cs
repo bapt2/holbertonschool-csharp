59d0dccad1ad291b3e87f8c6a8862aa1721200e0
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-28 00:44:58 +0200

    simple use of issubclassof

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Obj
+{
+    /// <summary>
+    /// public static bool IsOnlyASubclass
+    /// </summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType)) { return true; }
+        return false;
+    }
+}

0f449cbb3da611f7af7f5a2bd7f055a03b506421
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-28 01:58:35 +0200

    basics shape

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class Shape
+{
+    /// <summary>
+    /// public virtual int Area
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+

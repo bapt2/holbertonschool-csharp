fbdbee5a5be1cb5a1b2596cd0139ced051ab538c
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-28 00:28:42 +0200

    Is

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    /// <summary>
+    /// public static bool IsOfTypeInt
+    /// </summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj is int) { return true; }
+        return false;
+    }
+}
+

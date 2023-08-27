f4e021f136f0c3d4d4d456d09ff7bee3326e8077
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-28 00:37:39 +0200

    simple use of typeof and isinstanceoftype

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    /// <summary>
+    /// public static bool IsInstanceOfArray
+    /// </summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (typeof(Array).IsInstanceOfType(obj)) { return true; }
+        return false;
+    }
+}

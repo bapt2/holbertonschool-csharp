595726ed04b52c2dc188f366ea5f596afa64de2b
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-28 02:34:37 +0200

    full rectangle

---
+++ csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+class Shape
+{
+    /// <summary>
+    /// public virtual int Area
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    int width;
+    int height;
+
+    public int Width
+    {
+        get => width;
+        set
+        {
+            if (width < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+    public int Height
+    {
+        get => height;
+        set
+        {
+            if (width < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+    public override int Area()
+    {
+        return width * height;
+    }
+    public override string ToString()
+    {
+        return string.Format("[Rectangle] {0} / {1}", width, height);
+    }
+}

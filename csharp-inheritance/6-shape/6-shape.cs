835528dc11cd3c2c5b240662740144c0622cfa18
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-28 02:16:39 +0200

    Rectangle

---
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+class Shape
+{
+    /// <summary>
+    /// public virtual int Area
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    int width;
+    int height;
+
+    public int Width
+    {
+        get => width
+        set
+        {
+            if (width <= 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0")
+            }
+            set = width
+        }
+    }
+    public int Height
+    {
+        get => height
+        set
+        {
+            if (width <= 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0")
+            }
+            set = height
+        }
+    }
+}

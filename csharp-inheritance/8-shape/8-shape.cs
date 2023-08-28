75fb8c33ad6ed26eeca1c9a75a121512f03a93a9
bapt2 <baptiste18c.touraud@gmail.com>
2023-08-28 02:48:33 +0200

    square

---
+++ csharp-inheritance/8-shape/8-shape.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+class Shape
+{
+    /// <summary>
+    /// public virtual int Area
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    int width;
+    int height;
+
+    public int Width
+    {
+        get => width;
+        set
+        {
+            if (width < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+    public int Height
+    {
+        get => height;
+        set
+        {
+            if (height < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+    public override int Area()
+    {
+        return width * height;
+    }
+    public override string ToString()
+    {
+        return string.Format("[Rectangle] {0} / {1}", width, height);
+    }
+}
+
+class Square : Rectangle
+{
+    int size;
+
+    public int Size
+    {
+        get => size;
+        set
+        {
+            if (size < 0)
+            {
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            }
+            size = value;
+            Height = value;
+            Width = value;
+
+        }
+    }
+}

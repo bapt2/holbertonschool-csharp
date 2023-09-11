4e90d85c2b3b34f65016f4af62f29fb77ff2bbe8
bapt2 <baptiste18c.touraud@gmail.com>
2023-09-11 17:06:30 +0200

    trying to fix issue

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -3,7 +3,7 @@
 class MatrixMath
 {
     /// <summary>
-    /// 
+    /// public static double[,] Multiply
     /// </summary>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
@@ -12,16 +12,16 @@ class MatrixMath
             double[,] error = new double[,] {{ -1 }};
             return error;
         }
-        int column = matrix1.GetLength(0);
-        int row = matrix1.GetLength(1);
-        double[,] result = new double[column, row];
+        int row = matrix2.GetLength(0);
+        int column = matrix1.GetLength(1);
+        double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
         
-        for (int i = 0; i < column; i++)
+        for (int i = 0; i < row; i++)
         {
-            for (int j = 0; j < row; j++)
+            for (int j = 0; j < column; j++)
             {
                 result[i, j] = 0;
-                for (int k = 0; k < 2; k++)
+                for (int k = 0; k < column; k++)
                 {
                     result[i, j] += matrix1[i, k] * matrix2[k, j];
                 }

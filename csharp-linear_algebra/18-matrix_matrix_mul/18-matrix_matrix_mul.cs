22de7cdd0882c1e94f38f85677d6373203f847e5
bapt2 <baptiste18c.touraud@gmail.com>
2023-09-11 17:22:47 +0200

    fixing issue

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -12,20 +12,20 @@ class MatrixMath
         {
             return error;
         }
-        int row = matrix2.GetLength(0);
         int column = matrix1.GetLength(1);
+        int row = matrix2.GetLength(0);
         if (row != column)
         {
             return error;
         }
         double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
 
-        for (int i = 0; i < row; i++)
+        for (int i = 0; i < matrix1.GetLength(0); i++)
         {
-            for (int j = 0; j < column; j++)
+            for (int j = 0; j < matrix2.GetLength(1); j++)
             {
                 result[i, j] = 0;
-                for (int k = 0; k < column; k++)
+                for (int k = 0; k < matrix2.GetLength(1); k++)
                 {
                     result[i, j] += matrix1[i, k] * matrix2[k, j];
                 }

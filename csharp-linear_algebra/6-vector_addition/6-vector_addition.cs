c350bcea0c1855134ae91449eed7def7ffbd4760
bapt2 <baptiste18c.touraud@gmail.com>
2023-09-11 12:39:22 +0200

    adding two vector

---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class VectorMath
+{
+    /// <summary>
+    /// public static double[] Add
+    /// </summary>
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        double[] result = new double[vector1.Length];
+
+        if (vector1.Length < 2 || vector1.Length > 3 || vector2.Length < 2 || vector2.Length > 3)
+        {
+            double[] error = new double[] {-1};
+            return error;
+        }
+
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            result[i] = vector1[i] + vector2[i];
+        }
+        return result;
+    }
+}
+

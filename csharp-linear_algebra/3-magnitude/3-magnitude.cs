b0a4a6f650073d47bacbd690aaf78d7346c927d9
bapt2 <baptiste18c.touraud@gmail.com>
2023-09-11 11:59:44 +0200

    test

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        double result = 0;
+        if (vector.Length > 3 || vector.Length < 2)
+        {
+            return -1;
+        }
+        if (vector.Length == 3)
+        {
+            result = Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2));
+            return Math.Round(vector, 2); 
+        }
+        else
+        {
+            result = Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2));
+            return Math.Round(vector, 2); 
+        }
+    }
+}

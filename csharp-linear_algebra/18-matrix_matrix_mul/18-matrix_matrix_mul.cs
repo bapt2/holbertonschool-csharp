462f33aee1cb31c1ab8dc8aa34429f5250e21fa3
bapt2 <baptiste18c.touraud@gmail.com>
2023-09-11 17:15:33 +0200

    test

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -7,15 +7,19 @@ class MatrixMath
     /// </summary>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
+        double[,] error = new double[,] { { -1 } };
         if (matrix1.Length == 0 || matrix2.Length == 0)
         {
-            double[,] error = new double[,] {{ -1 }};
             return error;
         }
         int row = matrix2.GetLength(0);
         int column = matrix1.GetLength(1);
+        if (row != column)
+        {
+            return error;
+        }
         double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
-        
+
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < column; j++)

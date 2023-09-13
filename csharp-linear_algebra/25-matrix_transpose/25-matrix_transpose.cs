0d2c7aeb6f604cdb57b15d7806aa88abf277dacb
bapt2 <baptiste18c.touraud@gmail.com>
2023-09-13 12:33:46 +0200

    test

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                result[j, i] = matrix[i, j];
+            }
+        }
+    }
+}

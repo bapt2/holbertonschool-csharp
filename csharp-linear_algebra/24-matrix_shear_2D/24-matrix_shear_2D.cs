3f0240589be4f5641e3071a6439a2a97c6c2bdd9
bapt2 <baptiste18c.touraud@gmail.com>
2023-09-13 09:03:09 +0200

    testing something

--- csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -5,28 +5,29 @@ class MatrixMath
     public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
         double[,] error = { { -1 } };
-        if (matrix.Length == 0 || direction != 'x' && direction != 'y' || matrix.GetLength(0) != 2 || matrix.GetLength(1))
+        if (matrix.Length == 0 || matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        {
+            return error;
+        }
+        if (direction != 'x' && direction != 'y')
         {
             return error;
         }
         double[,] result = new double[2, 2];
-        if (direction == 'x')
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                result[i, j] = 0;
+                for (int k = 0; k < matrix.GetLength(0); k++)
                 {
-                    result[i, j] = 0;
-                    for (int k = 0; k < matrix.GetLength(0); k++)
+                    if (direction == 'x')
                     {
-                        if (direction == 'x')
-                        {
-                            result[i, j] += matrix[i, k] + factor * matrix[k, j];
-                        }
-                        if (direction == 'y')
-                        {
-                            result[i, j] += matrix[k, j] + factor * matrix[i, k];
-                        }
+                        result[i, j] = matrix[i, k] + (factor * matrix[k, j]);
+                    }
+                    else
+                    {
+                        result[i, j] = matrix[k, j] + (factor * matrix[i, k]);
                     }
                 }
             }

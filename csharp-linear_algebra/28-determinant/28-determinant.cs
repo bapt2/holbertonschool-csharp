6d22a4729432742d9cf3c4010f443ef5e69c2177
bapt2 <baptiste18c.touraud@gmail.com>
2023-09-14 09:26:17 +0200

    test

---
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+class MatrixMath
+{
+    /// <summary>
+    /// public static double Determinant
+    /// </summary>
+    public static double Determinant(double[,] matrix)
+    {
+        if (matrix.Length != 4 && matrix.Length != 9)
+        {
+            return -1;
+        }
+        double result = 0;
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (direction == matrix.Length == 4)
+                {
+                    result = (matrix[i, 0] * matrix[j, 0]) - (matrix[i, 1] * matrix[j, 1]);
+                    result[i, 1] = matrix[i, 1];
+                }
+
+            }
+        }
+        return result;
+    }
+}

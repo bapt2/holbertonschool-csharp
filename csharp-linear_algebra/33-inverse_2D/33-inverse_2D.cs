a1b986444b3d2810af56bcefe8a05a6fb00420fe
bapt2 <baptiste18c.touraud@gmail.com>
2023-09-14 12:44:35 +0200

    fixing last issue

--- csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -7,12 +7,18 @@ class MatrixMath
     /// </summary>
     public static double[,] Inverse2D(double[,] matrix)
     {
-        double[,] result = new double[2, 2];
-        double factor = 1 / ((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]));
-        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2 || factor == 0)
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
         {
             return new double[,] {{-1}};
         }
+
+        double factor = ((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]));
+        if (factor == 0)
+        {
+            return new double[,] {{-1}};
+        }
+        factor = 1 / ((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]));
+        double[,] result = new double[2, 2];
 
         result[0, 0] = Math.Round(matrix[1, 1] * factor, 2);
         result[0, 1] = Math.Round(-matrix[0, 1] * factor, 2);

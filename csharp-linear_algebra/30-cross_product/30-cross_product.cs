0631d6639ea957cd4e5e67760db781db59ca50b3
bapt2 <baptiste18c.touraud@gmail.com>
2023-09-14 11:04:55 +0200

    fixing last issue

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -13,7 +13,7 @@ class VectorMath
         }
         double[] result = new double[vector1.Length];
         result[0] = (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]);
-        result[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[0]);
+        result[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]);
         result[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
         return result;
     }

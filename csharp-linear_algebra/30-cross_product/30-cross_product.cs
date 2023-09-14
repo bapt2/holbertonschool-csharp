30f22fcd0a22cc4b7fc62e71742fc248b9c04637
bapt2 <baptiste18c.touraud@gmail.com>
2023-09-14 10:59:44 +0200

    adding a method that calculate the cross product of a vector

---
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class VectorMath
+{
+    /// <summary>
+    /// public static double[] CrossProduct
+    /// </summary>
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length < 3 || vector1.Length > 3 || vector1.Length != vector2.Length)
+        {
+            return new double[] {-1};
+        }
+        double[] result = new double[vector1.Length];
+        result[0] = (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]);
+        result[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[0]);
+        result[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
+        return result;
+    }
+}

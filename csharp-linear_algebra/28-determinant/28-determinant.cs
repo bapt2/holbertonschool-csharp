7f0e97e7e87d11a85744e997d48a37d79dfbdae3
bapt2 <baptiste18c.touraud@gmail.com>
2023-09-14 10:40:13 +0200

    improve my code

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -8,28 +8,26 @@ class MatrixMath
     /// </summary>
     public static double Determinant(double[,] matrix)
     {
-        if (matrix.Length != 4 && matrix.Length != 9)
-        {
-            return -1;
-        }
         double result = 0;
         double first = 0;
         double second = 0;
         double third = 0;
-
-        for (int i = 0; i < matrix.GetLength(1); i++)
+        
+        if (matrix.Length != 4 && matrix.Length != 9)
         {
-            if (matrix.Length == 4)
-            {
-                result = Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2);
-            }
-            else
-            {
-                first = (matrix[0, 0]) * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2]) * matrix[2, 1]);
-                second = (matrix[0, 1]) * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2]) * matrix[2, 0]);
-                third = (matrix[0, 2]) * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1]) * matrix[2, 0]);
-                result = Math.Round(first - second + third, 2);
-            }
+            return -1;
+        }
+
+        if (matrix.Length == 4)
+        {
+            result = Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2);
+        }
+        else
+        {
+            first = (matrix[0, 0]) * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2]) * matrix[2, 1]);
+            second = (matrix[0, 1]) * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2]) * matrix[2, 0]);
+            third = (matrix[0, 2]) * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1]) * matrix[2, 0]);
+            result = Math.Round(first - second + third, 2);
         }
         return result;
     }

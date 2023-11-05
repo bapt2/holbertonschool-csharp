61c33050103869367e58e142c912ee443325890a
bapt2 <5711@holbertonstudents.com>
2023-11-06 00:35:57 +0100

    adding Queue class

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Queue<T>
+{
+    /// <summary>
+    /// public Type CheckType()
+    /// </summary>
+    /// <returns></returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}

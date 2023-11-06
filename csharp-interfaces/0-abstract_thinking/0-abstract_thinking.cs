0d1baf9a2dfcf08266017e02b04aca344f7e2204
bapt2 <5711@holbertonstudents.com>
2023-11-06 20:04:56 +0100

    overriding ToString method

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+abstract class Base
+{
+    /// <summary>
+    /// public string name
+    /// </summary>
+    public string name {get; set;}
+
+    /// <summary>
+    /// public override string ToString
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return String.Format("{0} is a {1}", name, this.GetType());
+    }
+}

ec4ba5fa029eb5665dfd56e8352f11de9f05e5c9
bapt2 <5711@holbertonstudents.com>
2023-11-06 01:22:21 +0100

    adding new method

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Queue<T>
+{
+    Node head;
+    Node tail;
+    int count;
+
+    /// <summary>
+    /// public Type CheckType()
+    /// </summary>
+    /// <returns></returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary>
+    /// public class Node
+    /// </summary>
+    public class Node
+    {
+        T value;
+        public Node next {get; set;} = null;
+
+        /// <summary>
+        /// public Node(T Value)
+        /// </summary>
+        /// <param name="Value"></param>
+        public Node(T Value)
+        {
+            value = Value;
+        }
+    }
+
+    /// <summary>
+    /// public void EnQueue
+    /// </summary>
+    /// <param name="Value"></param>
+    public void Enqueue(T Value)
+    {
+        Node newNode = new Node(Value);
+
+        if (head == null)
+        {
+            tail = head = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+
+    /// <summary>
+    /// public int Count
+    /// </summary>
+    /// <returns></returns>
+    public int Count()
+    {
+        return count;
+    }
+}

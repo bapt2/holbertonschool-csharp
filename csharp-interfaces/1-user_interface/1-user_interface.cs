cae40b4154995463361a21e8205d7ac3672877df
bapt2 <5711@holbertonstudents.com>
2023-11-06 20:21:02 +0100

    adding interface

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+abstract class Base
+{
+    public string name {get; set;}
+
+    /// <summary>
+    /// public override string ToString
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return String.Format("{0} is a {1}", name, this.GetType());
+    }
+}
+
+public interface IInteractive
+{
+    /// <summary>
+    /// public void Interact
+    /// </summary>
+    public void Interact()
+    {
+
+    }
+}
+
+public interface IBreakable
+{
+    public int durability {get; set;}
+
+    /// <summary>
+    /// public void Break
+    /// </summary>
+    public void Break()
+    {
+
+    }
+}
+
+public interface ICollectable
+{
+    public bool isCollected {get; set;}
+
+    public void Collect()
+    {
+
+    }
+}
+
+public class TestObject: Base, IInteractive, IBreakable, ICollectable
+{
+    
+}
